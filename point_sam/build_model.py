6a46c6401c01c5cc5cf6e71159cdc53797d57507
LilRedWu <1353099226@qq.com>
2025-01-23 01:23:10 +0100

    build pt_sam

--- point_sam/build_model.py
+++ point_sam/build_model.py
@@ -20,4 +20,12 @@ def build_point_sam(checkpoint, num_group=256, group_size=64):
     mask_decoder = MaskDecoder(256, decoder_transformer)
     model = PointCloudSAM(encoder, mask_encoder, mask_decoder)
     load_model(model, checkpoint)
+    patch_embed = PatchEmbed(6, 512, num_group, group_size)
+    transformer = timm.create_model("eva02_large_patch14_448", pretrained=False)
+    encoder = PointCloudEncoder(patch_embed, transformer, 256)
+    mask_encoder = MaskEncoder(256)
+    decoder_transformer = TwoWayTransformer(2, 256, 8, 2048)
+    mask_decoder = MaskDecoder(256, decoder_transformer)
+    model = PointCloudSAM(encoder, mask_encoder, mask_decoder)
+    load_model(model, checkpoint)
     return model
